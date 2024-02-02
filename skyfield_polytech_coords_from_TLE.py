6688ee0cbfa56347722eac4bf9250aa5e29f5eba
davletshin_a_f <artemdavletshin12@gmail.com>
2024-02-02 16:01:58 +0300

    POLYTECH-3 coords from TLE added

---
+++ skyfield_polytech_coords_from_TLE.py
@@ -0,0 +1,22 @@
+from skyfield.api import *
+
+ts = load.timescale()
+t = ts.now()
+
+#eph = skyfield.api.load('de421.bsp')
+stations_url = 'http://celestrak.org/NORAD/elements/gp.php?INTDES=2023-091'
+satellites = load.tle_file(stations_url)
+print('Loaded', len(satellites), 'satellites')
+by_name = {sat.name: sat for sat in satellites}
+satellite = by_name['POLYTECH-UNIVERSE 3 (R*)']
+print(satellite)
+position = satellite.at(t)
+print(f'POLYTECH-UNIVERSE 3 (R*): {position.position.km}')
+
+
+lat, lon = wgs84.latlon_of(position)
+height = wgs84.height_of(position)
+
+print('Latitude:', lat.degrees)
+print('Longitude:', lon.degrees)
+print('Height:', height.km)
